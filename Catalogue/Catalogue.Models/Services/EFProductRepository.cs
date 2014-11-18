8af0b45b54771a80971fd8f1bdbff294a4700547
DeathWinged <mitkobfb2@gmail.com>
2014-11-18 16:10:16 +0200

    Setup complete! (I think so...)

---
+++ Catalogue/Catalogue.Models/Services/EFProductRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Catalogue.Models.Abstract;
+using Catalogue.Models.Entities;
+
+namespace Catalogue.Models.Services
+{
+    public class EFProductRepository : IProductRepository
+    {
+        public IEnumerable<Product> Products { get; set; }
+    }
+}
---
+++ Catalogue/Catalogue.Web/Controllers/ProductController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Catalogue.Models.Entities;
+using Catalogue.Models.Services;
+using Catalogue.Models.Abstract;
+
+
+namespace Catalogue.Web.Controllers
+{
+    public class ProductController : Controller
+    {
+        private IProductRepository repository;
+
+        public ProductController(IProductRepository productRepository)
+        {
+            this.repository = productRepository;
+        }
+
+        public ActionResult Index()
+        {
+            return View(repository.Products);
+        }
+    }
+}

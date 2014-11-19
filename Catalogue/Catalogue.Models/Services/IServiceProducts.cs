4fb2752d3721914cfbf9e85ec643d6a5a1b90da6
DeathWinged <mitkobfb2@gmail.com>
2014-11-19 17:27:19 +0200

    Services

---
+++ Catalogue/Catalogue.Models/Services/IServiceProducts.cs
@@ -0,0 +1,15 @@
+﻿using Catalogue.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catalogue.Models.Services
+{
+    interface IServiceProducts
+    {
+        void Add(Product product);
+
+    }
+}
---
+++ Catalogue/Catalogue.Models/Services/ServiceProducts.cs
@@ -0,0 +1,18 @@
+﻿using Catalogue.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catalogue.Models.Services
+{
+    class ServiceProducts : ICatalogueContext, IServiceProducts
+    {
+
+        public void Add(Product product)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

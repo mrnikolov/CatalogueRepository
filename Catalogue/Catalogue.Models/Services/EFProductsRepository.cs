4ce11d6948e6f67cf750af11f77dad5d5c5a4d43
DeathWinged <mitkobfb2@gmail.com>
2014-11-19 11:44:21 +0200

    Commit about repository...

--- Catalogue/Catalogue.Models/Services/EFProductsRepository.cs
+++ Catalogue/Catalogue.Models/Services/EFProductsRepository.cs
@@ -11,5 +11,39 @@ namespace Catalogue.Models.Services
     public class EFProductsRepository<T> : IRepository<T> where T : class
     {
 
+        public IQueryable<T> All()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T GetByID(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Add(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Detach(T entity)
+        {
+            throw new NotImplementedException();
+        }
     }
 }

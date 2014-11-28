69ff983ff2f666f6d271dcaf793d1aaabaacc24c
Svetoslav Georgiev <svetoslav.g@mail.com>
2014-11-28 17:26:00 +0200

    asd

---
+++ Catalogue/Catalogue.Models/Services/Contracts/IManufacturerService.cs
@@ -0,0 +1,27 @@
+﻿using Catalogue.Models.Entities;
+using Catalogue.Models.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catalogue.Models.Services
+{
+    public interface IManufacturerService
+    {
+        IEnumerable<Manufacturer> GetAll();
+
+        PagedList<Manufacturer> GetItems(int? page);
+
+        Manufacturer Find(int? id);
+
+        void Add(Manufacturer category);
+
+        void Modify(Manufacturer category);
+
+        void Remove(Manufacturer category);
+
+        void Remove(int id);
+    }
+}
---
+++ Catalogue/Catalogue.Web/Controllers/ProductController.cs
@@ -0,0 +1,79 @@
+﻿using Catalogue.Models.Entities;
+using Catalogue.Models.Services;
+using Catalogue.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Catalogue.Web.Controllers
+{
+    public class ProductController : Controller
+    {
+        private IProductService productServices;
+
+        public ProductController(IProductService productServices)
+        {
+            this.productServices = productServices;
+        }
+
+        [HttpGet]
+        public ActionResult Save()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult Save(ProductViewModel model, Image img)
+        {
+            if (ModelState.IsValid)
+            {
+                var images = new List<Image>();
+
+                foreach (var item in model.Files)
+                {
+                    if(item != null)
+                    {
+                        RenderImage service = new RenderImage();
+                        service.RenderImg(img, item);
+                        var rndrResizedImage = service.ResizeFile(img);
+                        images.Add(rndrResizedImage);
+                    }
+              
+                }
+
+                var product = new Product()
+                {
+                    Category = model.Category,
+                    CategoryID = model.CategoryID,
+                    Description = model.Description,
+                    Manufacturers = model.Manufacturers,
+                    Name = model.Name,
+                    ProductYear = model.ProductYear,
+                    Images = images
+                };
+
+                productServices.Add(product);
+            }
+            return View();
+        }
+
+        public ActionResult GetYears()
+        {
+            List<int> years = new List<int>();
+            var currentYear = DateTime.Now.Year;
+            years.Add(currentYear + 1);
+
+            for (int i = 0; i < 30; i++)
+            {
+                years.Add(currentYear - i);
+            }
+
+            return PartialView("_YearsPartial", years);
+        }
+    }
+}
